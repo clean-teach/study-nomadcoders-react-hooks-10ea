3d48eb0438ebea8a3d3baf854e4bf10f06cd5ee3
clean-teach <82921898+clean-teach@users.noreply.github.com>
2024-03-15 00:22:02 +0900

    #2.2 useClick 코드 수정
    - 콘솔창을 열지 않아도, 화면상에서 변화가 보이게 수정하였다.
    - 사실, 이번 수업의 요점은 코드가 변하는 것 자체가 아니라, 원하는 요소에 원하는 이벤트 리스너를 추가하는 것이다.

--- src/useClick/index.js
+++ src/useClick/index.js
@@ -1,10 +1,12 @@
+import { useState } from "react";
 import { useClick } from "./useClick";
 
 function UseClickComponent() {
-  const sayHello = () => console.log("Hello!");
+  const [toggle, setToggle] = useState(true);
+  const sayHello = () => setToggle((toggle) => !toggle);
   const clickRef = useClick(sayHello);
 
-  return <p ref={clickRef}>나를 클릭 해보슈</p>;
+  return <p ref={clickRef}>{toggle ? "나를 클릭 해보슈" : "다시 해보슈"}</p>;
 }
 
 export default UseClickComponent;
